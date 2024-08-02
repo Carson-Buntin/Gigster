e9e438036a4bc97e05e2e7768e0acb2863f2bd89
Carson-Buntin <carsonbuntin@gmail.com>
2024-08-02 00:15:53 -0500

    Added gigster logo to favicon and banner

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../Assets/music_note.png'
+import logo from '../Assets/Gigster-Logo.png'
 import './Header.css'
 
 
